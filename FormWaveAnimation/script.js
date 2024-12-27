f71d2bd605550fb3a36f8f706f1b99f111de37f2
mboriuch <michal123borciuch@gmail.com>
2024-12-27 13:04:12 +0100

    Finished FormWaveAnimation

--- FormWaveAnimation/script.js
+++ FormWaveAnimation/script.js
@@ -1,1 +1,7 @@
+const labels = document.querySelectorAll(".form-control label")
 
+labels.forEach(label => {
+    label.innerHTML = label.innerText.split("")
+        .map((character, index) => `<span style="transition-delay: ${index * 50}ms" ">${character}</span>`)
+        .join("")
+})
